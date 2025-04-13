dae9fdf6e41ea05456471fe1b52b9ddfbeb8688e
Abir-dev <abirlalbanerjee44@gmail.com>
2025-04-13 18:34:58 +0530

    created feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,8 +1,12 @@
-import { db } from "@/firebase/admin"
+'use server'
+
+import { feedbackSchema } from "@/constants";
+import { db } from "@/firebase/admin";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
 
 export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
-    const interviews = await db
-    .collection('interviews')
+    const interviews = await db.collection('interviews')
     .where('userId', '==', userId)
     .orderBy('createdAt', 'desc')
     .get();
@@ -18,9 +22,9 @@ export async function getLatestInterviews(params: GetLatestInterviewsParams): Pr
 
     const interviews = await db
     .collection('interviews')
-    .orderBy('createdAt', 'desc')
-    .where('finalized', '==', true)
     .where('userId', '!=', userId)
+    .where('finalized', '==', true)
+    .orderBy('createdAt', 'desc')
     .limit(limit)
     .get();
 
@@ -30,12 +34,93 @@ export async function getLatestInterviews(params: GetLatestInterviewsParams): Pr
     })) as Interview[];
 }
 
-
 export async function getInterviewById(id: string): Promise<Interview | null> {
     const interview = await db
     .collection('interviews')
-   .doc(id)
-   .get();
+    .doc(id)
+    .get();
 
     return interview.data() as Interview | null;
+}
+
+export async function CreateFeedback(params: CreateFeedbackParams) {
+  const { interviewId, userId, transcript } = params;
+
+  try {
+    const formattedTranscript = transcript
+      .map((sentence: { role: string; content: string }) => `- ${sentence.role}: ${sentence.content}\n`)
+      .join('');
+
+    const generated = await generateObject({
+      model: google('gemini-2.0-flash-001', {
+        structuredOutputs: false,
+      }),
+      schema: feedbackSchema,
+      prompt: `
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+      `,
+      system:
+        "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+    });
+
+    if (!generated || !generated.object) {
+      throw new Error('Invalid response from generateObject');
+    }
+
+    const {
+      totalScore,
+      categoryScores,
+      strengths,
+      areasForImprovement,
+      finalAssessment,
+    } = generated.object;
+
+    const feedback = await db.collection('feedback').add({
+      interviewId,
+      userId,
+      totalScore,
+      categoryScores,
+      strengths,
+      areasForImprovement,
+      finalAssessment,
+      createdAt: new Date().toISOString(),
+    });
+
+    return {
+      success: true,
+      feedbackId: feedback.id,
+    };
+  } catch (e) {
+    console.error('Error saving feedback', e);
+    return { success: false };
+  }
+}
+
+export async function GetFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
+    const { interviewId, userId } = params;
+
+    const feedback = await db
+    .collection('feedback')
+    .where('interviewId', '==', interviewId)
+    .where('userId', '==', userId)
+    .orderBy('createdAt', 'desc')
+    .limit(1)
+    .get();
+
+    if(feedback.empty) return null;
+
+    const feedbackDoc = feedback.docs[0];
+    return {
+        id: feedbackDoc.id,
+        ...feedbackDoc.data(),
+    } as Feedback;
 }
